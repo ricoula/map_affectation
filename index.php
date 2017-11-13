422068b38c2d6a59b8b8f053a91e0665db4da720
Cyril RICOU <cyril.ricou@Ambition.local>
2017-11-13 15:10:35 +0100

    map

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
+<?php include("header.php") ?>
 <!DOCTYPE html>
 <html>
   <head>
     <title></title>
+    <link rel="stylesheet" type="text/css" href="CSS/style.css">
   </head>
   <body>
 
+     <div id="map"></div>
+
+  <?php include("footer.php") ?>
   </body>
 </html>
