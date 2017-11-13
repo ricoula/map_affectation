d6cc2f3cfc27ac923c190e1426a2048e6760882d
Cyril RICOU <cyril.ricou@Ambition.local>
2017-11-13 18:06:15 +0100

    ez

--- index.php
+++ index.php
@@ -9,7 +9,15 @@
   </head>
   <body>
   <div id="test"></div>
-    <div id="map_content"><span id="glyph">X</span><div id="map"></div></div>
+    <div id="map_content"><div id="glyph">
+        <div class="glyph_div glyph_div_border"><span id="glyph-1" class="glyphicon glyphicon-home font-glyph" aria-hidden="true"></span></div>
+        <div class="glyph_div glyph_div_border"><span id="glyph-2" class="glyphicon glyphicon-filter font-glyph" aria-hidden="true"></span></div>
+        <div class="glyph_div glyph_div_border"><span id="glyph-3" class="glyphicon glyphicon-user font-glyph" aria-hidden="true"></span></div>
+        <div class="glyph_div"><span id="glyph-4" class="glyphicon glyphicon-inbox font-glyph" aria-hidden="true"></span></div>
+
+    </div>
+         <div id="map"></div>
+    </div>
     
   <?php include("footer.php") ?>
   </body>
