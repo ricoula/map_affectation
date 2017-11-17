1fb7e8c9a8441f01f0db548dd43b176f72d6d0a3
Cyril RICOU <cyril.ricou@Ambition.local>
2017-11-17 10:04:23 +0100

    ez

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,6 @@
 $(function(){
 
     $("#slide-box").click(function(){
-        
         $("#side_bar").animate({left:'0px'},500);
         $("#glyph").animate({left:'500px'},500);
         $("#side_bar").load('slide-box.php');
@@ -16,11 +15,15 @@ $(function(){
         $("#slide-filter").addClass("active");
     });
     $("#slide-home").click(function(){
+        var poi = getUrlParameter('poi');
         $("#side_bar").animate({left:'0px'},500);
         $("#glyph").animate({left:'500px'},500);
         $("#side_bar").load('slide-home.php');
         $(".glyph_div").removeClass("active");
         $("#slide-home").addClass("active");
+        if(poi != undefined){
+            $("#side_bar").load('slide-home.php?poi_id=' + poi);
+        }
     });
     $("#slide-users").click(function(){
         $("#side_bar").animate({left:'0px'},500);
@@ -29,5 +32,19 @@ $(function(){
         $(".glyph_div").removeClass("active");
         $("#slide-users").addClass("active");
     });
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+    
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+    
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    };
 
 });
