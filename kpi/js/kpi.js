9ac2864d44a9732fe64b2d6eab9db0b4a452a9ad
Cyril RICOU <cyril.ricou@Ambition.local>
2018-01-10 12:38:57 +0100

    ez

--- kpi/js/kpi.js
+++ kpi/js/kpi.js
@@ -3,7 +3,7 @@ $(function(){ //DOM Ready
     var gridster = $(".gridster ul").gridster({widget_margins: [3, 3], widget_base_dimensions: [140, 160]}).data('gridster');
     
     $("#ajouterCase").click(function(){
-        gridster.add_widget('<li class="new"><div class="menuCase"><a href="#" class="glyphicon glyphicon-resize-small"></a> <a href="#" class="glyphicon glyphicon-resize-full"></a> <a href="#" class="glyphicon glyphicon-fullscreen"></a></div></li>', 1, 1);
+        gridster.add_widget('<li class="new"><div class="menuCase"><span href="#" class="glyphicon glyphicon-resize-small"></span><span href="#" class="glyphicon glyphicon-resize-full"></span><span href="#" class="glyphicon glyphicon-fullscreen"></span><span href="#" class="glyphicon glyphicon-remove"></span></div></li>', 1, 1);
     });
 
     
