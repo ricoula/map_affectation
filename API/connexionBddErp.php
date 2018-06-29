76144bae9ffafed0cf69864ab2e1e59584dab3a4
Florian Spadaro <spadaro.florian@outlook.fr>
2018-06-29 15:45:18 +0200

    les compétences apparaissent désormais sous la forme d'une seule icône. Lorsque l'on passe la souris sur cette icône, on voit la liste des compétences. Réglage problème des caffs qui n'ont pas d'UI (problème lors de l'echo de l'UI)

--- API/connexionBddErp.php
+++ API/connexionBddErp.php
@@ -3,7 +3,7 @@
 	
 	try{
 		if(!isset($_SESSION['simu']) || $_SESSION['simu'] == false){
-			$bddErp = new PDO('pgsql:host=192.168.30.240;dbname=ambigroup_dev', 'admambigroup', '13jkgaUM8Um');
+			$bddErp = new PDO('pgsql:host=192.168.30.240;dbname=ambigroup_prod8', 'admambigroup', '13jkgaUM8Um');
 		}
 		else{
 			$bddErp = new PDO('pgsql:host=192.168.30.218;dbname=openerp', 'CYRRIC', 'cyril');
